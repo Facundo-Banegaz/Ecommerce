efd91d9db565ed0e9f9933d65c929a49798b0421
Facundo-Banegaz <Banegaz@users.noreply.github.com>
2025-01-14 16:32:07 -0300

    Se agrego la clase Country y se hizo el crud.

---
+++ Ecommerce/Ecommerce/Data/DataContext.cs
@@ -0,0 +1,27 @@
+﻿using Ecommerce.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Ecommerce.Data
+{
+    public class DataContext: DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options)  : base(options)
+        {    
+        }
+
+
+        public DbSet<Country> Countries { get; set; }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+
+            modelBuilder.Entity<Country>().HasIndex(c => c.Name).IsUnique();
+
+        }
+
+
+    }
+}
---
+++ Ecommerce/Ecommerce/Data/Entities/Country.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Ecommerce.Data.Entities
+{
+    public class Country
+    {
+        public Guid Id { get; set; }
+
+        [Display(Name = "Pais")]
+        [Required(ErrorMessage ="El Campo {0} es obligatorio.")]
+        [MaxLength(50,ErrorMessage ="El Campo {0} debe tener maximo {1} caractéres.")]
+        
+        public string Name { get; set; }
+
+
+
+    }
+}
