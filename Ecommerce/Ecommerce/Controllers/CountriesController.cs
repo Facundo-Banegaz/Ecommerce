0ece307f2a995278cbdddc5e302d58decdfc4101
Facundo-Banegaz <Banegaz@users.noreply.github.com>
2025-01-15 19:47:16 -0300

    se agregaron vistas parciales a Country y se validaron los datos para que existan valores duplicados en la db.

--- Ecommerce/Ecommerce/Controllers/CountriesController.cs
+++ Ecommerce/Ecommerce/Controllers/CountriesController.cs
@@ -58,13 +58,32 @@ namespace Ecommerce.Controllers
         {
             if (ModelState.IsValid)
             {
-                country.Id = Guid.NewGuid();
-                _context.Add(country);
-                await _context.SaveChangesAsync();
-                return RedirectToAction(nameof(Index));
+                try
+                {
+                    country.Id = Guid.NewGuid();
+                    _context.Add(country);
+                    await _context.SaveChangesAsync();
+                    return RedirectToAction(nameof(Index));
+                }
+                catch (DbUpdateException dbUpdateException)
+                {
+                    if (dbUpdateException.InnerException.Message.Contains("duplicate"))
+                    {
+                        ModelState.AddModelError(string.Empty, "Ya existe un país con el mismo nombre.");
+                    }
+                    else
+                    {
+                        ModelState.AddModelError(string.Empty, dbUpdateException.InnerException.Message);
+                    }
+                }
+                catch (Exception exception)
+                {
+                    ModelState.AddModelError(string.Empty, exception.Message);
+                }
             }
             return View(country);
         }
+ 
 
         // GET: Countries/Edit/5
         public async Task<IActionResult> Edit(Guid? id)
@@ -100,19 +119,25 @@ namespace Ecommerce.Controllers
                 {
                     _context.Update(country);
                     await _context.SaveChangesAsync();
+                    return RedirectToAction(nameof(Index));
                 }
-                catch (DbUpdateConcurrencyException)
+                catch (DbUpdateException dbUpdateException)
                 {
-                    if (!CountryExists(country.Id))
+                    if (dbUpdateException.InnerException.Message.Contains("duplicate"))
                     {
-                        return NotFound();
+                        ModelState.AddModelError(string.Empty, "Ya existe un país con el mismo nombre.");
                     }
                     else
                     {
-                        throw;
+                        ModelState.AddModelError(string.Empty, dbUpdateException.InnerException.Message);
                     }
                 }
-                return RedirectToAction(nameof(Index));
+                catch (Exception exception)
+                {
+                    ModelState.AddModelError(string.Empty, exception.Message);
+                }
+   
+               
             }
             return View(country);
         }
@@ -150,9 +175,5 @@ namespace Ecommerce.Controllers
             return RedirectToAction(nameof(Index));
         }
 
-        private bool CountryExists(Guid id)
-        {
-            return _context.Countries.Any(e => e.Id == id);
-        }
     }
 }
