dd0e293bd78f5cac4f823cd387de7b381a779c87
Facundo-Banegaz <Banegaz@users.noreply.github.com>
2025-02-23 01:08:30 -0300

    pantalla de inicio super  básica

--- Ecommerce/Ecommerce/Controllers/HomeController.cs
+++ Ecommerce/Ecommerce/Controllers/HomeController.cs
@@ -1,22 +1,58 @@
 using System.Diagnostics;
+using Ecommerce.Data;
+using Ecommerce.Data.Entities;
 using Ecommerce.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace Ecommerce.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly   DataContext _context;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, DataContext context)
         {
             _logger = logger;
+            _context = context;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            return View();
+            List<Product>? products = await _context.Products
+                .Include(p => p.ProductImages)
+                .Include(p => p.ProductCategories)
+                .OrderBy(p => p.Description)
+                .ToListAsync();
+            List<ProductsHomeViewModel> productsHome = new() { new ProductsHomeViewModel() };
+            int i = 1;
+            foreach (Product? product in products)
+            {
+                if (i == 1)
+                {
+                    productsHome.LastOrDefault().Product1 = product;
+                }
+                if (i == 2)
+                {
+                    productsHome.LastOrDefault().Product2 = product;
+                }
+                if (i == 3)
+                {
+                    productsHome.LastOrDefault().Product3 = product;
+                }
+                if (i == 4)
+                {
+                    productsHome.LastOrDefault().Product4 = product;
+                    productsHome.Add(new ProductsHomeViewModel());
+                    i = 0;
+                }
+                i++;
+            }
+
+            return View(productsHome);
         }
+
         public IActionResult Contact()
         {
             return View();
---
+++ Ecommerce/Ecommerce/Models/ProductsHomeViewModel.cs
@@ -0,0 +1,16 @@
+﻿using Ecommerce.Data.Entities;
+
+namespace Ecommerce.Models
+{
+    public class ProductsHomeViewModel
+    {
+        public Product Product1 { get; set; }
+
+        public Product Product2 { get; set; }
+
+        public Product Product3 { get; set; }
+
+        public Product Product4 { get; set; }
+
+    }
+}
