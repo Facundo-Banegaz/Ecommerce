06650fdbfb2bbd067d4ef8e935b39e79f58a39b9
Facundo-Banegaz <Banegaz@users.noreply.github.com>
2025-04-18 21:48:37 -0300

    Se empezo con orders creando el controlador y algunas vistas

---
+++ Ecommerce/Ecommerce/Controllers/OrdersController.cs
@@ -0,0 +1,54 @@
+﻿using Ecommerce.Data;
+using Ecommerce.Data.Entities;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Ecommerce.Controllers
+{
+    [Authorize(Roles = "Admin")]
+    public class OrdersController : Controller
+    {
+        private readonly DataContext _context;
+
+        public OrdersController(DataContext context)
+        {
+            _context = context;
+        }
+        public async Task<IActionResult> Index()
+        {
+
+
+            var res = await _context.Orders
+                .Include(s => s.User)
+                .Include(s => s.OrderDetails)
+                .ThenInclude(sd => sd.Product)
+                .ToListAsync();
+
+
+            return View(res);
+        }
+
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Order sale = await _context.Orders
+                .Include(s => s.User)
+                .Include(s => s.OrderDetails)
+                .ThenInclude(sd => sd.Product)
+                .ThenInclude(p => p.ProductImages)
+                .FirstOrDefaultAsync(s => s.Id == id);
+            if (sale == null)
+            {
+                return NotFound();
+            }
+
+            return View(sale);
+        }
+
+    }
+}
